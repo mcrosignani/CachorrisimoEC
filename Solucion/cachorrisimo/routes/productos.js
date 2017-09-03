c2bb19c8f1066524104aac3d4132ee05575aef2c
MCrosignani <crosignani.mauro@gmail.com>
2017-09-03 02:07:19 -0300

    20170902-Continuo mejorando el index

--- Solucion/cachorrisimo/routes/productos.js
+++ Solucion/cachorrisimo/routes/productos.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cachorrisimo' });
-});
-
 /* GET products page. */
 router.get('/productos', function(req, res, next) {
   res.render('productos', { title: 'Cachorrisimo' });
