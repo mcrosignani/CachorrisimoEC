252b5e7c693adeaaa8615f9232483aadff15de65
MCrosignani <crosignani.mauro@gmail.com>
2018-07-15 23:06:52 -0300

    Cachorrisimo con angular

---
+++ Solucion/cachorrisimo2/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Route } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'products', component: ProductsComponent }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
