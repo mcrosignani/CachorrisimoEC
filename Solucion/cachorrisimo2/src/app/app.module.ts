dae52f5d37322fdd2795c3f9bad22c71a5cac49b
mcrosignani <crosignani.mauro@gmail.com>
2018-07-19 15:45:27 -0300

    20180719_Footer

--- Solucion/cachorrisimo2/src/app/app.module.ts
+++ Solucion/cachorrisimo2/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer-component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
