ec84bdd069a370af244315909cec8e39cc3aff2b
mcrosignani <crosignani.mauro@gmail.com>
2018-07-20 16:29:31 -0300

    20180720_AvanzoEn VistaProductos

--- Solucion/cachorrisimo2/src/app/products/products.component.ts
+++ Solucion/cachorrisimo2/src/app/products/products.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-products',
-    templateUrl: './products.component.html'
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
     
